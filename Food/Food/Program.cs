37d7bdc43024e152453c26e454d32556e308bdc3
Non't <90344104+Shef905015@users.noreply.github.com>
2023-12-12 11:23:36 +0100

    Add files via upload

---
+++ Food/Food/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Food
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foodType foodType = new foodType();
+
+            foodType foodType1 = new foodType("Banana", 55, 66, 5);
+            foodType foodType2 = new foodType("Jabuka", 69, 69, 69);
+            foodType foodType3 = new foodType("Pašta", 100, 250, 50);
+
+            Console.WriteLine(foodType.getCounter());
+
+            Console.WriteLine(foodType1.ToString());
+            Console.WriteLine(foodType2.ToString());
+            Console.WriteLine(foodType3.ToString());
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Food/Food/foodType.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Food
+{
+    internal class foodType
+    {
+        string name;
+        int protein;
+        int carbs;
+        int fat;
+        static int counter;
+
+        public foodType(string name, int protein, int carbs, int fat)
+        {
+            this.name = name;
+            this.protein = protein;
+            this.carbs = carbs;
+            this.fat = fat;
+            counter++;
+        }
+        public foodType() { counter++; }
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+
+        public void setProtein(int protein)
+        {
+            this.protein = protein;
+        }
+
+        public void setCarbs(int carbs)
+        {
+            this.carbs = carbs;
+        }
+
+        public void setFat(int fat)
+        {
+            this.fat = fat;
+        }
+
+        public string getName()
+        {
+            return this.name;
+        }
+        public int getProtein()
+        {
+            return this.protein;
+        }
+        public int getCarbs()
+        {
+            return this.carbs;
+
+        }
+        public int getFat()
+        {
+            return this.fat;
+        }
+        public int getCounter()
+        {
+            return counter;
+        }
+
+        public override string ToString()
+        {
+            string ispis = "ime: " + this.getName() + "\nprotein: " + this.getProtein() + "%\n" + "carbs: " + this.getCarbs() + "%" + "\nfat: " + this.getFat() + "%\n";
+            return ispis;
+        }
+
+    }
+}
